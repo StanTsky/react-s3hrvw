6d0bcbca8867cb95f955ac9cf8acab0f6aff1cbb
StanTsky <stanislavturovsky@lewisu.edu>
2021-01-30 01:04:17 -0600

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,16 +3,11 @@ import "./style.css";
 
 export default function App() {
   return (
-    <style>
-.content {
-  max-width: 500px;
-  margin: auto;
-}
-</style>
+    
     <div class="content">
 	<img src="CalendarLogo.png" alt="Logo"/>
 	<br /><br />
-	<hr border: 450px solid Black; border-radius: 100px>
+
 
       <label for="username"> Username:</label>
       <input type="text" id="fname" name="fname" />
